b6af7eef5ed2f5983d52ccbab7efcf04e04d0071
ELIEL DOUNA <elielreich@yahoo.com>
2022-10-18 11:58:35 -0300

    changes

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
-* _putchar writes character to stdout  
-* @c: charater to be printed
-* return: 1 if succeful and -1 if not 
+* _putchar - function to print
+* @c: chararter to print
+* return: 1 if succeful and -1 if not
 */
 
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
